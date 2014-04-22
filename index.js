ba9897de155f60a1b508710edc9cf1b3ce29f17f
Ritwik Dutta <ritzymail@gmail.com>
2014-04-22 16:08:00 -0700

    Eh

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
 var stage = new PIXI.Stage(0x000000);
  
-    // create a renderer instance.
-    var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
- 
-    // add the renderer view element to the DOM
-    document.body.appendChild(renderer.view);
- 
+var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
+
+
+document.body.appendChild(renderer.view);
+
+requestAnimFrame( animate );
+
+
+var texture = PIXI.Texture.fromImage("assets/back.jpg");
+
+var back = new PIXI.Sprite(texture);
+
+back.width = window.innerWidth;
+back.height = window.innerHeight;
+
+stage.addChild(back);
+
+function animate() {
+
     requestAnimFrame( animate );
- 
-    // create a texture from an image path
-    var texture = PIXI.Texture.fromImage("assets/bunny.png");
-    // create a new Sprite using the texture
-    var bunny = new PIXI.Sprite(texture);
- 
-    // center the sprites anchor point
-    bunny.anchor.x = 0.5;
-    bunny.anchor.y = 0.5;
 
+    back.rotate += 0.1;
 
- 	bunny.x = window.innerWidth/2;
-    bunny.y = window.innerHeight/2;
-
-
-    stage.addChild(bunny);
- 
-    function animate() {
- 
-        requestAnimFrame( animate );
- 
-        bunny.rotate += 0.1;
- 
-        // render the stage   
-        renderer.render(stage);
-    }
+    renderer.render(stage);
+}
